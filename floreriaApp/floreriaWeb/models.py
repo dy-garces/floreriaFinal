507b2660d547b6c7a78c7c92f8250836827bbbf0
Roberto Fuentealba <ro.fuentealba@duocuc.cl>
2022-06-06 10:55:03 -0400

    Formularioavance

--- floreriaApp/floreriaWeb/models.py
+++ floreriaApp/floreriaWeb/models.py
@@ -15,7 +15,7 @@ class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     precio = models.IntegerField()
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.TextField(max_length=200)
     imgen = models.ImageField(upload_to="productos",null=True)
     categoria =  models.ForeignKey(Categoria, on_delete=models.PROTECT)
     
