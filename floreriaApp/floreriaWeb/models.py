0124221f1fd104720648488646a06a561ae678bc
Nestor <nes.valenzuela@duocuc.cl>
2022-06-06 12:02:59 -0400

    a ver si pasa

--- floreriaApp/floreriaWeb/models.py
+++ floreriaApp/floreriaWeb/models.py
@@ -47,14 +47,14 @@ class Cliente(models.Model):
     comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
     
     def __str__(self):
-        return self.nombre
+        return ""+self.rut_cliente
 
 class Subcripcion(models.Model):
     num_sub = models.IntegerField(primary_key=True)
     descuento = models.IntegerField()
     
     def __str__(self):
-        return self.num_sub
+        return str(self.num_sub)
     
 class Estado_subcripcion(models.Model):
     hora = datetime.now()
@@ -62,7 +62,7 @@ class Estado_subcripcion(models.Model):
     num_sub = models.ForeignKey(Subcripcion , on_delete=models.PROTECT)
     
     def __str__(self):
-        return self.hora
+        return str(self.num_sub)+" "+str(self.rut_cliente)
     
 class Vendedor(models.Model):
     rut_vendedor = models.CharField(max_length=10,primary_key=True)
@@ -90,10 +90,10 @@ class Seguimiento_Compra(models.Model):
     id_seguimiento = models.IntegerField(primary_key=True)
     descripcion = models.CharField(max_length=50)
     def __str__(self):
-        return self.id_seguimiento
+        return self.descripcion
     
 class Venta(models.Model):
-    num_venta = models.IntegerField(primary_key=True)
+    num_venta = models.AutoField(primary_key=True)
     hora = datetime.now()
     fecha = models.DateField()
     total = models.IntegerField()
@@ -103,7 +103,7 @@ class Venta(models.Model):
     id_seguimiento = models.ForeignKey(Seguimiento_Compra, on_delete= models.PROTECT)
     
     def __str__(self):
-        return self.num_venta
+        return str(self.num_venta)
 
 class Detalle_Venta(models.Model):
     cantidad = models.IntegerField()
@@ -115,8 +115,5 @@ class Detalle_Venta(models.Model):
     
     
     def __str__(self):
-        return self.cantidad
-    
-
-
+        return str(self.num_venta)+" "+str(self.id_producto)+" "+str(self.cantidad)
     
