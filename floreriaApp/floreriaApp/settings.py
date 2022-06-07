d7ef29e2f9810b5be45fd3958a492f7d6f03173b
Dyllan Garces <dy.garces@duocuc.cl>
2022-06-06 22:21:48 -0400

    fatality carrito 1

--- floreriaApp/floreriaApp/settings.py
+++ floreriaApp/floreriaApp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'crispy_forms',
     'crispy_bootstrap5',
+    'carro',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carro.context_processor.importe_total_carro',
             ],
         },
     },
